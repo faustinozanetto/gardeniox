d6d4cc84f378dbdd5a2c628797922c2f1893e750
Faustino Zanetto <faustizanetto@gmail.com>
2021-02-14 17:58:15 -0300

    Fixed api url

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -8,7 +8,10 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   }
 
   return {
-    url: 'http://localhost:4000/graphql',
+    url:
+      process.env.NODE_ENV === 'production'
+        ? 'https://gardeniox-server.herokuapp.com'
+        : 'http://localhost:4000/graphql',
     fetchOptions: {
       credentials: 'include' as const,
       headers: cookie ? { cookie } : undefined,
